c4150d264cfea8e0ba907c7abf7f30c152004e32
ori sharim <ori.sharim@gmail.com>
2025-01-17 11:30:09 +0200

    organized libs

--- Robot2025/src/main/java/frc/robot/RobotContainer.java
+++ Robot2025/src/main/java/frc/robot/RobotContainer.java
@@ -56,26 +56,26 @@ public class RobotContainer {
     configureBindings();
 
     // Configure AutoBuilder last
-    // AutoBuilder.configure(
-    //         SwerveToWpi::getPos, // Robot pose supplier
-    //         SwerveToWpi::resetPos, // Method to reset odometry (will be called if your auto has a starting pose)
-    //         SwerveToWpi::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-    //         SwerveToWpi::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-    //         new HolonomicPathFollowerConfig(
-    //                 new PIDConstants(2.5, 0.0, 0.0), // Translation PID constants
-    //                 new PIDConstants(2.5, 0.0, 0.0), // Rotation PID constants
-    //                 2,
-    //                 SwerveConsts.ROBOT_RADIUS,
-    //                 new ReplanningConfig(true, false)
-    //         ),
-    //         () -> {
-    //           // Boolean supplier that controls when the path will be mirrored for the red alliance
-    //           // This will flip the path being followed to the red side of the field.
-    //           // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-    //           return  Robot.getAlliance() == DriverStation.Alliance.Red;
-    //         },
-    //         Swerve.getInstance() // Reference to this subsystem to set requirements
-    // );
+    AutoBuilder.configure(
+            SwerveToWpi::getPos, // Robot pose supplier
+            SwerveToWpi::resetPos, // Method to reset odometry (will be called if your auto has a starting pose)
+            SwerveToWpi::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            SwerveToWpi::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+            new HolonomicPathFollowerConfig(
+                    new PIDConstants(2.5, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(2.5, 0.0, 0.0), // Rotation PID constants
+                    2,
+                    SwerveConsts.ROBOT_RADIUS,
+                    new ReplanningConfig(true, false)
+            ),
+            () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+              return  Robot.getAlliance() == DriverStation.Alliance.Red;
+            },
+            Swerve.getInstance() // Reference to this subsystem to set requirements
+    );
 
     
   }
